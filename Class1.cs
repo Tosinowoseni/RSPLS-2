d5742b7f53546613754692f8178a989605e7e7a6
tosinowoseni <tosinowosen1@icloud.com>
2022-04-28 17:30:09 -0400

    Made Modifications to Class and Program files

--- Class1.cs
+++ Class1.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RSPLS2
+namespace RPSLS_CS
 {
-    class Computer : Player
+    internal class Player
     {
-        public Computer()
+
+        //member variables
+        public List<string> gestures;
+        public int score;
+
+        //constructor
+        public Player()
         {
-            this.name = "Computer";
+            this.score = 0;
+            this.gestures = new List<string>()
+            {
+                new string("rock"),
+                new string("paper"),
+                new string("scissors"),
+                new string("lizard"),
+                new string("spock"),
+            };
         }
 
+        //membermethods
+        public virtual int ChooseGesture()
+        {
+            return 0;
+        }
     }
 }
---
+++ program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            RunGame game = new RunGame();
+
+            //Human tessa = new Human();
+
+            //tessa.ChooseGesture();
+
+            //Robot robot = new Robot();
+
+            //robot.ChooseGesture();
+        }
+    }
+}
